7df81ed2f43ac41933fadf05512d4cd7f8d29ff8
Rikii999 <151664607+Rikii999@users.noreply.github.com>
2026-03-05 12:15:51 +0200

    only html

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "realestate";
-
-$conn = new mysqli($host, $user, $password, $database);
-if($conn->connect_error) die("Ошибка подключения: ".$conn->connect_error);
-?>
